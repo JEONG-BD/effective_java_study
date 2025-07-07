95d28d53a76a87c1f2959521afa73ad8297c629d
JEONG-BD <dev.byeongdu@gmail.com>
2025-07-08 08:29:01 +0900

    hashCode

--- src/main/java/org/effective/chapter02/item11/hashcode/PhoneNumber.java
+++ src/main/java/org/effective/chapter02/item11/hashcode/PhoneNumber.java
@@ -1,10 +1,12 @@
 package org.effective.chapter02.item11.hashcode;
 
+import org.effective.chapter02.item10.Point;
+
 import java.util.Objects;
 
 public final class PhoneNumber {
     private final short areaCode, prefix, lineNum;
-
+    private Point point;
     public PhoneNumber(int areaCode, int prefix, int lineNum) {
         this.areaCode = rangeCheck(areaCode, 999, "area code");
         this.prefix = rangeCheck(prefix, 999, "prefix");
@@ -30,9 +32,19 @@ public final class PhoneNumber {
                 && phoneNumber.areaCode == areaCode;
     }
 
+    //@Override
+    //public int hashCode() {
+    //    return 42;
+    //    //return Objects.hash(areaCode, prefix, lineNum);
+    //}
+
     @Override
-    public int hashCode() {
-        return 42;
-        //return Objects.hash(areaCode, prefix, lineNum);
+    public int hashCode(){
+        // primitive
+        int result = Short.hashCode(areaCode);
+        result = 31 * result + Short.hashCode(prefix);
+        result = 31 * result + Short.hashCode(lineNum);
+
+        return result;
     }
 }
